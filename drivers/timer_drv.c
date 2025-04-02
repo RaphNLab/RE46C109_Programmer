afa5b02c656c8811ea809605f81a9afb1bb35ca1
RaphNLab <s.ngoufack@raphnlab.de>
2025-04-02 23:34:21 +0200

    Solving merge conflict in  main.c, uart_dvr.c, timer_dvr.c and timer_dvr.h
    Signed-off-by: RaphNLab <s.ngoufack@raphnlab.de>

--- drivers/timer_drv.c
+++ drivers/timer_drv.c
@@ -1,14 +1,12 @@
 #include "timer_drv.h"
-
 #include <libopencm3/stm32/timer.h>
 
 
-/**
- * Data type, Constant and macro definitions
- *
-*/
+void sleep_config(void)
+{
+	rcc_clock_setup_pll(&rcc_clock_config[RCC_CLOCK_VRANGE1_HSI_PLL_24MHZ]);
+	rcc_periph_clock_enable(RCC_TIM2);
 
-<<<<<<< HEAD
 	timer_set_period(TIM2, 5000); /* Generate timer overflow evet every 2ms */
 	timer_set_prescaler(TIM2, 24); /* Counter increment every 1us */
 	timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
@@ -26,22 +24,6 @@ void timer3_init(void)
 	timer_enable_irq(TIM3, TIM_DIER_UIE);
 	timer_enable_counter(TIM3);
 }
-=======
-/**
- * Static data declaration
- *
-*/
-
-/**
- * Private function prototypes
- *
-*/
-
-/**
- * Public functions
- *
-*/
->>>>>>> refs/remotes/origin/main
 
 void sleep_us(uint16_t delay_us)
 {
@@ -57,34 +39,5 @@ void sleep_ms(uint16_t delay_ms)
 		sleep_us(1000);
 	}
 }
-
-void timer_sleep_config(void)
-{
-	rcc_clock_setup_pll(&rcc_clock_config[RCC_CLOCK_VRANGE1_HSI_PLL_24MHZ]);
-	rcc_periph_clock_enable(RCC_TIM2);
-
-	timer_set_period(TIM2, 5000); // Generate timer overflow event every 2ms
-	timer_set_prescaler(TIM2, 24); // Counter increment every 1us
-	timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-	timer_enable_counter(TIM2);
-}
-
-/*Configure timer 3 for debouncing*/
-void timer_debounce_config(void)
-{
-	rcc_periph_clock_enable(RCC_TIM3);
-	nvic_enable_irq(NVIC_TIM3_IRQ);
-
-	timer_set_prescaler(TIM3, 24000);
-	timer_set_period(TIM3, 20);
-	timer_set_mode(TIM3, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-	timer_enable_irq(TIM3, TIM_DIER_UIE);
-}
-
-
-/**
- * Private functions
- *
-*/
 
 
--- main.c
+++ main.c
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
 
 #include "drivers/RE46C109_drv.h"
-=======
->>>>>>> refs/remotes/origin/main
 #include "drivers/timer_drv.h"
 #include "drivers/uart_drv.h"
 #include "drivers/led_driver.h"
-<<<<<<< HEAD
-#include "drivers/button.h"
-
-int main(void)
-{
-	timer_sleep_config();
-	timer_debounce_config();
-
-	button_config();
-	led_config();
-
-	while (1)
-	{
-<<<<<<< HEAD
-=======
 #include "drivers/uart_drv.h"
 
 
@@ -37,21 +19,7 @@ int main(void)
 	{
 		UartHandleCmd_Task(&myUartDev);
 
->>>>>>> develop
-		re46c109_setPrameter(config_reg);
-=======
-		if(button_debounce(GREEN_BUTTON))
-		{
-			//gpio_set(GPIOA, GPIO5);
-			gpio_clear(GPIOA, GPIO5);
-			//gpio_toggle(GPIOA, GPIO5);
-		}
-		else if (button_debounce(GREEN_BUTTON))
-		{
-			//gpio_clear(GPIOA, GPIO5);
-			gpio_set(GPIOA, GPIO5);
-		}
->>>>>>> refs/remotes/origin/main
+		re46c109_runModeT0(config_reg);
 	}
 	return (0);
 }
